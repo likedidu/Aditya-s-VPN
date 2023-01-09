ad941d5cd7ba608bea1476d58203a8bb7f0db6fb
likedidu <91383267+likedidu@users.noreply.github.com>
2023-01-09 20:39:29 +0800

    1

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,43 +3,34 @@
 if [[ -z "${Password}" ]]; then
   Password="5c301bb8-6c77-41a0-a606-4ba11bbab084"
 fi
-ENCRYPT="chacha20-ietf-poly1305"
-QR_Path="/qr"
 
 #V2Ray Configuration
-V2_Path="/v2"
-mkdir /wwwroot
-mv /v2 /usr/bin/v2
+Server_Path="/adwwqAqd"
 
-if [ ! -d /etc/shadowsocks-libev ]; then  
-  mkdir /etc/shadowsocks-libev
+mkdir /wwwroot
+
+mv /v2-server /usr/bin/v2-server
+
+if [ ! -d /etc/server ]; then  
+  mkdir /etc/server
 fi
 
-# TODO: bug when PASSWORD contain '/'
 sed -e "/^#/d"\
     -e "s/\${PASSWORD}/${Password}/g"\
-    -e "s/\${ENCRYPT}/${ENCRYPT}/g"\
-    -e "s|\${V2_Path}|${V2_Path}|g"\
-    /conf/shadowsocks-libev_config.json >  /etc/shadowsocks-libev/config.json
-echo /etc/shadowsocks-libev/config.json
-cat /etc/shadowsocks-libev/config.json
+    -e "s|\${Server_Path}|${Server_Path}|g"\
+    /conf/server-config.json >  /etc/server/config.json
+
+echo /etc/server/config.json
+
+cat /etc/server/config.json
 
 sed -e "/^#/d"\
     -e "s/\${PORT}/${PORT}/g"\
-    -e "s|\${V2_Path}|${V2_Path}|g"\
-    -e "s|\${QR_Path}|${QR_Path}|g"\
+    -e "s|\${Server_Path}|${Server_Path}|g"\
     -e "$s"\
-    /conf/nginx_ss.conf > /etc/nginx/conf.d/ss.conf 
+    /conf/nginx.conf > /etc/nginx/conf.d/ss.conf 
 
-if [ "${Domain}" = "no" ]; then
-  echo "Aditya's Personal VPN"
-else
-  plugin=$(echo -n "v2ray;path=${V2_Path};host=${Domain};tls" | sed -e 's/\//%2F/g' -e 's/=/%3D/g' -e 's/;/%3B/g')
-  ss="ss://$(echo -n ${ENCRYPT}:${Password} | base64 -w 0)@${Domain}:443?plugin=${plugin}" 
-  echo "${ss}" | tr -d '\n' > /wwwroot/index.html
-  echo -n "${ss}" | qrencode -s 6 -o /wwwroot/vpn.png
-fi
+v2-server run -c /etc/server/config.json &
 
-ss-server -c /etc/shadowsocks-libev/config.json &
 rm -rf /etc/nginx/sites-enabled/default
 nginx -g 'daemon off;'
